400a1aff97249198763318514b1a58433707d2d1
Verccety <verccety1@gmail.com>
2020-09-11 19:28:35 +0700

    Final version as it seems

--- script.js
+++ script.js
@@ -1,96 +1,101 @@
+let myLibrary = JSON.parse(localStorage.getItem('myLibrary')) || [];
 const container = document.querySelector(".card-container");
+
+const closePopUp = document.getElementsByTagName('span')[0];
+closePopUp.addEventListener('click', () => document.querySelector(".bg-container").style.display = "none");
+
 const btnBook = document.querySelector(".add-book");
+btnBook.addEventListener("click", () => {
+  document.querySelector(".bg-container").style.display = "flex";
+});
+
 const btnAdd = document.querySelector("#addbook");
-let myLibrary = [];
-//addBookToLibrary(new Book("Monte Cristo", "Duma", 1600, true))
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+btnAdd.addEventListener("click", () => {
+  document.querySelector(".bg-container").style.display = "none";
+  addBookToLibrary(new Book());
+  form.reset();
+});
+
+function Book() {
+  this.title = form.title.value;
+  this.author = form.author.value;
+  this.pages = form.pages.value;
+  this.read = form.isread.checked;
 }
 
 function addBookToLibrary(obj) {
   myLibrary.push(obj);
+  saveToStorage();
   addOneBook();
 }
 
 function isRead(el) {
-  el.previousElementSibling.textContent = myLibrary[el.id-1].read = `${el.checked ? "Read" : "Not read"}`
+  myLibrary[el.id].read = !myLibrary[el.id].read;
+  el.previousElementSibling.textContent = `${el.checked ? "Read" : "Not read"}`
+  saveToStorage();
 }
 
-function createBooks() {
-  // while (container.firstChild) {
-  //   container.removeChild(container.firstChild)
-  // }
+function saveToStorage() {
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+}
+
+function displayBooks() {
+    while (container.childElementCount > 1) {
+    container.removeChild(container.lastChild)
+  }
   myLibrary.forEach(addOneBook);
 }
-createBooks();
+
 function addOneBook(lastItem, index) {
-  // while (container.firstChild) {
-  //   container.removeChild(container.firstChild)
-  // }
   if (lastItem == null) {
     lastItem = myLibrary[myLibrary.length - 1]
-    index = myLibrary.length
+    index = myLibrary.length - 1
   }
 
-    const div = document.createElement("div");
+  const div = document.createElement("div");
+  const h2 = document.createElement("h2");
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+  const p3 = document.createElement("p");
+  const newCheckBox = document.createElement("input");
+  const newCheckBox1 = document.createElement("button");
+
     div.classList.add(`card-box`);
     div.setAttribute("data-book", `${index}`);
     container.appendChild(div);
 
-    const h2 = document.createElement("h2");
-    h2.textContent = `${lastItem.title}`;
+    h2.textContent = `${lastItem.title}, [${index}]`;
     div.appendChild(h2);
 
-    const p1 = document.createElement("p");
     p1.textContent = `Author: ${lastItem.author}`;
     div.appendChild(p1);
 
-    const p2 = document.createElement("p");
     p2.textContent = `Pages: ${lastItem.pages}`;
     div.appendChild(p2);
-
-    const p3 = document.createElement("p");
+    
     p3.textContent = `${lastItem.read ? "Read" : "Not read"}`;
     div.appendChild(p3);
-
-    const newCheckBox = document.createElement("input");
+    
     newCheckBox.type = "checkbox";
     newCheckBox.checked = lastItem.read;
     newCheckBox.setAttribute("onclick", `isRead(this)`);
     newCheckBox.setAttribute("id", `${index}`);
     div.appendChild(newCheckBox);
 
-    const newCheckBox1 = document.createElement("button");
     newCheckBox1.textContent = "Remove";
     newCheckBox1.classList.add(`remove-btn`);
     newCheckBox1.setAttribute("id", `${index}`);
     div.appendChild(newCheckBox1);
   }
 
-
-
-btnBook.addEventListener("click", (e) => {
-  document.querySelector(".bg-container").style.display = "flex";
-});
-
-btnAdd.addEventListener("click", (e) => {
-  let title = document.querySelector("input#title").value;
-  let author = document.querySelector("input#author").value;
-  let pages = document.querySelector("input#pages").value;
-  let isread = document.querySelector("input#isread").checked;
-  addBookToLibrary(new Book(title, author, pages, isread))
-  document.querySelector(".bg-container").style.display = "none";
-});
-
 document.body.addEventListener( 'click', e => {
   if (e.target.className === 'remove-btn') {
-    myLibrary.splice(e.target.id - 1, 1)
-    var el = document.querySelector(`[data-book="${e.target.id}"]`);
-    el.remove(); 
+    myLibrary.splice(e.target.id, 1);
+    saveToStorage();
+    let el = document.querySelector(`[data-book="${e.target.id}"]`);
+    el.remove();
+    displayBooks();
   }
 } );
 
-
+displayBooks();
